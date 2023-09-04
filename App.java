d158806eca4abc6901677b0b6aad6b69b842f0d6
LuisMoraga <142850705+LuisMoraga@users.noreply.github.com>
2023-09-04 17:35:40 -0600

    Add files via upload

---
+++ App.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        //Estructura switch
+        //Se usa una -> en lugar del :
+        //Para no necesitar poner la setencia break;
+        Scanner lector = new Scanner(System.in);
+        System.out.println("Color del semaforo: (r/a/v) ");
+        char color = lector.next().charAt(0);
+
+        switch (color) {
+         
+           case 'r' ->{
+                System.out.println("El semaforo esta en rojo. ");
+           }
+
+           case 'a' ->{
+                System.out.println("El semaforo esta en amarillo. ");
+           }
+
+           case 'v' ->{
+                System.out.println("El semaforo esta en verde. ");
+           }
+
+           default ->{
+
+           }
+             
+        }
+    }
+}
